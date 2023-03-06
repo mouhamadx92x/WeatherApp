25fa7125221b02d16e46eba7d312e50b7832a6b4
mouhamadx92x <mouhamadx92x@gmail.com>
2023-03-06 13:00:54 +0100

    Fixing

--- src/Context/Context.js
+++ src/Context/Context.js
@@ -1,11 +1,11 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useState } from "react";
 import axios from "axios";
 
 export const weatherContext = createContext();
 
 const WeatherProvider = (props) => {
   const [data, setData] = useState({});
-   const [location, setLocation] = useState("");
+  const [location, setLocation] = useState("");
 
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=919777219eec1ff8b9953ecf61212a36`;
 
@@ -13,10 +13,6 @@ const WeatherProvider = (props) => {
     const res = await axios.get(url);
     setData(res.data);
   };
-
-  
-
-
 
   // const fetchdate = async () => {
   //   const res = await fetch(url);
@@ -26,7 +22,9 @@ const WeatherProvider = (props) => {
   // };
 
   return (
-    <weatherContext.Provider value={ {data,location,setData,setLocation,fetchdate} }>
+    <weatherContext.Provider
+      value={{ data, location, setData, setLocation, fetchdate }}
+    >
       {props.children}
     </weatherContext.Provider>
   );
